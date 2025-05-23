8e5a06251e87eb72de9f5b880388730e36196316
SheralA16 <avegasheral@uss.edu.pe>
2025-05-23 02:28:54 -0500

    new cambios

--- sistema_compras/settings.py
+++ sistema_compras/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bd_compras',
         'USER': 'admin_compras',
-        'PASSWORD': 'Mon76810474',
+        'PASSWORD': 'admin_compras',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
