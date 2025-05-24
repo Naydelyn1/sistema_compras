f610fe663cabe8d79f44a9aa2b32429e6d478dc2
Naydelyn <mgarcian@uss.pe>
2025-05-24 02:52:13 -0500

    .............

--- sistema_compras/settings.py
+++ sistema_compras/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bd_compras',
         'USER': 'admin_compras',
-        'PASSWORD': '71749437',
+        'PASSWORD': 'Mon7681047',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
